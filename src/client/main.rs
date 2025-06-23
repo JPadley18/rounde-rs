905a8ac3b61ffa1e38bfb4d6fa792a4d22c7534e
Jacob Padley <jacob5180@hotmail.co.uk>
2025-06-23 20:23:22 +0100

    No more socket2, and working networking

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,24 @@
+use std::net::{TcpStream};
+use std::io::Write;
+use std::process;
+
 fn main() {
-    println!("Hello from the client");
+    println!("Trying to connect to server");
+    let mut socket = match TcpStream::connect("127.0.0.1:4444") {
+        Ok(sock) => sock,
+        Err(e) => {
+            eprintln!("Failed to connect to server: {e:?}");
+            process::exit(1);
+        },
+    };
+
+    println!("Connected!");
+
+    let message = "Hello World!";
+    match socket.write_all(message.as_bytes()) {
+        Err(e) => {
+            eprintln!("Failed to send data: {e:?}");
+        },
+        _ => (),
+    };
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,55 @@
+use std::io::Read;
+use std::net::{TcpStream, TcpListener, SocketAddr};
+use std::{thread, process};
+
 fn main() {
-    println!("Hello from the server");
+    println!("Starting server");
+    let socket = match TcpListener::bind("0.0.0.0:4444") {
+        Ok(sock) => sock,
+        Err(e) => {
+            eprintln!("Failed to initialise socket: {e:?}");
+            process::exit(1);
+        }
+    };
+
+    loop {
+        let incoming = socket.accept();
+        if incoming.is_err() {
+            let err = incoming.unwrap_err();
+            eprintln!("Incoming connection failed to establish properly {err:?}");
+            continue;
+        }
+
+        let (conn, addr) = incoming.unwrap();
+
+        // New connection
+        _ = thread::spawn(move || {
+            // Spawn a new worker thread to look after this socket
+            socket_worker(conn, addr);
+        });
+    }
+}
+
+fn socket_worker(mut socket: TcpStream, addr: SocketAddr) {
+    println!("New connection from {addr:?}");
+    loop {
+        // Read from the socket
+        let mut buffer = [0; 1024];
+        match socket.read(&mut buffer) {
+            Err(e) => {
+                println!("Connection with {addr:?} has failed: {e:?}");
+                return;
+            },
+            Ok(bytes) => {
+                if bytes > 0 {
+                    println!("{bytes:?} bytes from {addr:?}:");
+                } else {
+                    println!("Connection with {addr:?} closed");
+                    return;
+                }
+            },
+        }
+        let message = String::from_utf8_lossy(&buffer);
+        println!("{message}");
+    }
 }
